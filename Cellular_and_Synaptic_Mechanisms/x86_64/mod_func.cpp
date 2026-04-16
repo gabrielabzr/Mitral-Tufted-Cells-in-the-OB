62458798112af47829c9bc7daf8fb78a64600785
Gabriela Vitória <gabrielavitoriabzr@gmail.com>
2026-04-16 13:49:31 +0000

    ...

---
+++ Cellular_and_Synaptic_Mechanisms/x86_64/mod_func.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "hocdec.h"
+extern int nrnmpi_myid;
+extern int nrn_nobanner_;
+
+
+extern "C" void modl_reg() {
+  if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+    fprintf(stderr, "Additional mechanisms from files\n");
+    fprintf(stderr, "\n");
+  }
+}
